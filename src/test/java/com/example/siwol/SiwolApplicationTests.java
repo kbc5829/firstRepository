b40fa0e2edd1ce8f68bbc2ef0b1c2a3306e7b979
kbc5829 <65396428+kbc5829@users.noreply.github.com>
2021-10-03 14:14:02 +0900

    first commit
    first commit

---
+++ src/test/java/com/example/siwol/SiwolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siwol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/example/siwol/StringTest.java
@@ -0,0 +1,6 @@
+package com.example.siwol;
+
+public class StringTest {
+
+
+}
